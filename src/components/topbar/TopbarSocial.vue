ba4e28b313eb848e572aec46558e5797779fab63
cdryampi <cdryampi@gmail.com>
2025-06-02 15:54:56 +0200

    feat: add bounce effect to social links in TopbarSocial component

--- src/components/topbar/TopbarSocial.vue
+++ src/components/topbar/TopbarSocial.vue
@@ -5,7 +5,7 @@ import { socialLinks } from '@/data/socialLinks.js' // crea este archivo como hi
 <template>
    <ul class="flex items-center justify-center w-full max-w-3xl social-links " >
       <li class="inline-block mr-4" v-for="socialLinks in socialLinks" :key="socialLinks.id">
-         <a :href="socialLinks.href" class="text-white hover:tran" title="Follow us on {{ socialLinks.name }}">
+         <a :href="socialLinks.href" class="text-white hover:tran bounce-div" title="Follow us on {{ socialLinks.name }}">
             <svg class="icon icon-sm"
                viewBox="0 0 24 24" stroke="none" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                <path
